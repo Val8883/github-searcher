733b8fb58d0ced64d9bf22844015c627531a5322
Val <vcom@Vals-MacBook-Pro.local>
2022-02-02 13:44:01 +0100

    complite priject

---
+++ src/components/repos/RepoItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ReposItem(props) {
+  return (
+    <div>Repos Items</div>
+  );
+}
+
+export default ReposItem;
---
+++ src/components/repos/RepoList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReposItem from './ReposItem';
+
+function ReposList(props) {
+  return (
+    <ReposItem/>
+  );
+}
+
+export default ReposList;
